8ec94020fa7fee617e722c53c3d5c6c9f06d8240
johnsmccain <johnsdanlami@gmail.com>
2026-07-21 09:09:45 +0100

    Frontend class

---
+++ frontend-lectures/src/App.tsx
@@ -0,0 +1,46 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  sepolia,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+
+// configuration for wagmi and rainbowkit
+
+const config = getDefaultConfig({
+  appName: 'Blockchain App',
+  projectId: 'd69f9edb23bbe0354a3b186e1bee340c',
+  chains: [sepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+
+import './App.css'
+import Home from './Home';
+import Swap from './Swap';
+
+function App() {
+  const queryClient = new QueryClient();
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={
+        queryClient
+      }>
+        <RainbowKitProvider>
+          <Home />
+          <Swap />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default App
